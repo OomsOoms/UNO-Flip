897b1a1d06b60e01236f510666c74f9da52f2e58
Thomas <99562510+OomsOoms@users.noreply.github.com>
2023-10-30 01:17:03 +0000

    Adding forms to join game page

--- my-react-app/src/joinGame.js
+++ my-react-app/src/joinGame.js
@@ -1,39 +1,63 @@
+import React, { useState } from 'react';
 import "./joinGame.css"
 
-function UsernameInput() {
+function JoinGameForm() {
+  const [gameId, setGameId] = useState(''); // Use state to manage the gameId input
+
+  const handleGameIdChange = (event) => {
+    setGameId(event.target.value); // Update the gameId state when input changes
+  };
+
   return (
-    <input placeholder="Username" id="usernameInput"/>
+    <form action="/lobby">
+      <div>
+        <input type="text" placeholder="Username" id="usernameInput"/>
+      </div>
+      <div>
+        <input
+          placeholder="Game ID"
+          id="gameIdInput"
+          name="id" // Add a name attribute for the gameId input
+          value={gameId}
+          onChange={handleGameIdChange} // Update the gameId state when input changes
+        />
+        <button type="submit" id="joinGameButton">Join!</button>
+      </div>
+    </form>
   );
 }
 
-function GameIdInput() {
+function CreateGameForm() {
   return (
-    <input placeholder="Game ID" id="gameIdInput"/>
-  );
-}
-
-function JoinGameButton() {
-  return (
-    <button id="joinGameButton">Join Game</button>
+    <form>
+      <div>
+        <input placeholder="Username" id="usernameInput"/>
+      </div>
+      <div>
+        <button id="createGameButton">Create Game</button>
+      </div>
+    </form>
   );
 }
 
 function JoinGame() {
   return (
-    <div className="JoinGame">
-      <div>
+      <>
         <h1>Join Game</h1>
-      </div>
 
-      <div>
-        <UsernameInput />
-      </div>
+        <JoinGameForm />
 
-      <div>
-        <GameIdInput />
-        <JoinGameButton />
-      </div>
-    </div>
+        <p>or</p>
+        <h1>Create Game</h1>
+        <CreateGameForm />
+    
+        <div className="notification">
+          <div className="notification-content">
+            <span className="close-btn" >&times;</span>
+            <p id="notification-message">This is a notification message.</p>
+          </div>
+        </div>
+      </>
   );
 }
 
